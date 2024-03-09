8edbe9b658dca2cfc666b0b48c62351cee8e3b0b
ntritin62 <21522677@gm.uit.edu.vn>
2024-03-09 12:33:59 +0700

    section-2-23

--- section-2/investment_calculator.go
+++ section-2/investment_calculator.go
@@ -7,9 +7,11 @@ import (
 
 func main() {
 	const inflationRate = 6.5
-	var investmentAmount float64 = 1000.0
+	var investmentAmount float64
 	years := 10.0
 	expectedReturnRate := 5.5
+
+	fmt.Scan(&investmentAmount)
 
 	futureValue := investmentAmount * math.Pow(1 + expectedReturnRate / 100, years) 
 	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
