671ae26f7fd7770a7709dabede98521e4018d579
ntritin62 <21522677@gm.uit.edu.vn>
2024-03-10 01:09:30 +0700

    section-2-34

--- section-2/investment_calculator.go
+++ section-2/investment_calculator.go
@@ -42,9 +42,10 @@ func outputText(text string) {
 	fmt.Print(text)
 }
 
-func calculateFutureValues(investmentAmount float64, expectedReturnRate float64, years float64)  (float64, float64) {
+func calculateFutureValues(investmentAmount float64, expectedReturnRate float64, years float64)  (fv float64, rfv float64) {
 	
-	fv := investmentAmount * math.Pow(1 + expectedReturnRate / 100, years)
-	rfv := fv / math.Pow(1+inflationRate/100, years)
+	fv = investmentAmount * math.Pow(1 + expectedReturnRate / 100, years)
+	rfv = fv / math.Pow(1+inflationRate/100, years)
 	return fv, rfv
+	// return 
 }
