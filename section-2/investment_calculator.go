52fa06c9ea1f385a8fbeeced68618e8efa71a378
ntritin62 <21522677@gm.uit.edu.vn>
2024-03-09 22:38:17 +0700

    section-2-30

--- section-2/investment_calculator.go
+++ section-2/investment_calculator.go
@@ -23,7 +23,10 @@ func main() {
 	futureValue := investmentAmount * math.Pow(1 + expectedReturnRate / 100, years) 
 	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
 
+	formattedFV := fmt.Sprintf("Future Value: %.1f\n", futureValue)
+	formattedRFV := fmt.Sprintf("Future Value (adjusted for Inflation): %.1f\n",futureRealValue)
 	// fmt.Println("Future Value:",futureValue)
-	fmt.Printf("Future Value: %.1f\nFuture Value (adjusted for Inflation): %.1f", futureValue,futureRealValue)
+	// fmt.Printf("Future Value: %.1f\nFuture Value (adjusted for Inflation): %.1f", futureValue,futureRealValue)
 	// fmt.Println("Future Value (adjusted for Inflation):",futureRealValue)
+	fmt.Print(formattedFV, formattedRFV)
 }
