95e2bf82c1ee4bf704f8056737b6a57da250820e
ntritin62 <21522677@gm.uit.edu.vn>
2024-03-10 14:35:12 +0700

    section-2-35

--- section-2-practice-1/profit_calculator.go
+++ section-2-practice-1/profit_calculator.go
@@ -4,24 +4,31 @@ import "fmt"
 
 
 func main() {
-	var revenue float64 
-	var expenses float64
-	var taxRate float64
+	// var revenue float64 
+	// var expenses float64
+	// var taxRate float64
 
-	fmt.Print("Revenue: ")
-	fmt.Scan(&revenue)
+	revenue := getUserInput("Revenue: ")
+	expenses := getUserInput("Expenses: ")
+	taxRate := getUserInput("Tax Rate: ")
 
-	fmt.Print("Expenses: ")
-	fmt.Scan(&expenses)
+	ebt, profit, ratio := calculateFinancials(revenue, expenses, taxRate)
 
-	fmt.Print("Tax Rate: ")
-	fmt.Scan(&taxRate)
+	fmt.Printf("%.1f\n",ebt)
+	fmt.Printf("%.1f\n",profit)
+	fmt.Printf("%.3f\n",ratio)
+}
 
+func calculateFinancials(revenue, expenses, taxRate float64) (float64, float64, float64){
 	ebt := revenue - expenses
 	profit := ebt * (1 - taxRate/100)
 	ratio :=ebt/profit
+	return ebt, profit, ratio
+}
 
-	fmt.Println(ebt)
-	fmt.Println(profit)
-	fmt.Println(ratio)
+func getUserInput(infoText string) float64 {
+	var userInput float64
+	fmt.Print(infoText)
+	fmt.Scan(&userInput)
+	return userInput
 }
