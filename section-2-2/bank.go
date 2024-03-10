fe2dc7212fba71f570fd9b3e54c58ef3dbdb066b
ntritin62 <21522677@gm.uit.edu.vn>
2024-03-10 22:04:05 +0700

    section-2-37

--- section-2-2/bank.go
+++ section-2-2/bank.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 
 func main() {
+	var accountBalance = 1000.0
 	fmt.Println("Welcome to Go Bank!")
 	fmt.Println("What do you want to do?")
 	fmt.Println("1. Check balance")
@@ -11,7 +12,14 @@ func main() {
 	fmt.Println("4. Exit")
 
 	var choice int 
+	fmt.Print("Your choice: ")
 	fmt.Scan(&choice)
+
+	// wantsCheckBalance := choice == 1
+
+	if choice == 1 {
+		fmt.Println("Your balance is", accountBalance)
+	}
 
 	fmt.Println("Your choice:", choice)
 }
