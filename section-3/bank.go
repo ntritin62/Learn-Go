82472a8b437c0f340c68565b51f8bfea12ce4bef
ntritin62 <21522677@gm.uit.edu.vn>
2024-03-12 12:54:41 +0700

    section-3-60

--- section-3/bank.go
+++ section-3/bank.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 
 	"example.com/bank/fileops"
+
+	"github.com/Pallinder/go-randomdata"
 )
 
 const accountBalanceFile = "balance.txt"
@@ -21,6 +23,7 @@ func main() {
 	}
 
 	fmt.Println("Welcome to Go Bank!")
+	fmt.Println("Reach us 24/7",randomdata.PhoneNumber())
 
 	for {
 		presentOptions()
